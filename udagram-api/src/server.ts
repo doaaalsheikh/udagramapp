0214ff0e8b5f00ea5bb53bd25c7f5b1d3dee01c5
doaaalsheikh <doaa.alsheikh@gmail.com>
2022-01-31 04:54:57 +0200

    test route

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -33,7 +33,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/ Hello there");
+    res.send("/api/v0/ Hello From API");
   });
 
   // Start the Server
