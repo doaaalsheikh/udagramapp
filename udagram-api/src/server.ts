a51f4086a33252c101b91589afd1dc8a84b3e1fc
doaaalsheikh <doaa.alsheikh@gmail.com>
2022-02-01 05:33:32 +0200

    cors

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -22,12 +22,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }));
+  app.use(cors());
 
   app.use("/api/v0/", IndexRouter);
 
