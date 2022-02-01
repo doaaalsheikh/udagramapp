a0bea57884342b9c027a5b498cc05055692a5db5
doaaalsheikh <doaa.alsheikh@gmail.com>
2022-02-01 06:23:25 +0200

    add header

--- udagram-api/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api/src/controllers/v0/users/routes/user.router.ts
@@ -1,16 +1,22 @@
-import {Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 
-import {User} from '../models/User';
-import {AuthRouter} from './auth.router';
+import { User } from '../models/User';
+import { AuthRouter } from './auth.router';
 
 const router: Router = Router();
+
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
 
 router.use('/auth', AuthRouter);
 
 router.get('/');
 
 router.get('/:id', async (req: Request, res: Response) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const item = await User.findByPk(id);
   res.send(item);
 });
