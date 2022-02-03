9d7a346b0e4f97bef1b9aa905e91723de74db88b
doaaalsheikh <doaa.alsheikh@gmail.com>
2022-02-03 17:03:51 +0200

    add firefox

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-firefox-launcher'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
@@ -25,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'],
     singleRun: false
   });
 };
