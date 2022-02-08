99f9050c4f9dfab7eec67835ab88e1937b837865
doaaalsheikh <96584915+doaaalsheikh@users.noreply.github.com>
2022-02-08 04:15:47 +0200

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,2 @@
-
     eb deploy udagram-api-env --staged --profile default
     eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagramdb PORT=8080 DB_PORT=5432 POSTGRES_HOST=database-1.coxf48ajay33.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=my-udagramapp-bucket URL=http://my-udagramapp-bucket.s3-website-us-east-1.amazonaws.com JWT_SECRET=mysecretvaluefortoken
